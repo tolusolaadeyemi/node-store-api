9a7ce3f28d8532eafce6e14512693859e1c33e25
tolusolaadeyemi <tolusa@hotmail.com>
2022-07-18 13:28:04 +0100

    sigh

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use('/api/v1/products', products)
 app.use(notFoundMiddleware)
 app.use(errorMiddleware)
 
-const port = process.env.PORT 
+const port = process.env.PORT || 5000;
 
 const start = async () =>{
   try {
