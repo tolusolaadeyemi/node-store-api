ec1f89a5a76b53017aba9061c31057f07e0f70d9
tolusolaadeyemi <tolusa@hotmail.com>
2022-07-18 14:02:20 +0100

    hmm

--- app.js
+++ app.js
@@ -26,11 +26,13 @@ app.use('/api/v1/products', products)
 app.use(notFoundMiddleware)
 app.use(errorMiddleware)
 
+const port = process.env.PORT
+
 const start = async () =>{
   try {
     //connect DB
     await connectDB(process.env.MONGO_URI)
-    app.listen(PORT, console.log(`server is listening on port ${PORT}...`))
+    app.listen(port, console.log(`server is listening on port ${port}...`))
   } catch (error) {
     
   }  
