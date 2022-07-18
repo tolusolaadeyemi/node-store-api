0626599e3ce827a23f12da5ecefdae176f980f8d
tolusolaadeyemi <tolusa@hotmail.com>
2022-07-18 16:15:56 +0100

    port changed again

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use('/api/v1/products', products)
 app.use(notFoundMiddleware)
 app.use(errorMiddleware)
 
-const port = process.env.PORT || 5000
+let port = process.env.PORT || 5000
 
 const start = async () =>{
   try {
